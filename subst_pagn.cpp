6167b82756689d91cb603bc8f332acd99fb63f33
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-08-31 19:59:06 -0300

    :sparkles: feat: FIFO function

--- subst_pagn.cpp
+++ subst_pagn.cpp
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+int fifo(int pags[], int length, int num_frames) {
+    int frames[num_frames];
+    int falta_de_pag = 0;
+    int frame_index = 0;
+
+    for (int i = 0; i < num_frames; i++) {
+        frames[i] = -1;
+    }
+
+    for (int i = 0; i < length; i++) {
+        int pag = pags[i];
+        int found = 0;
+
+        for (int j = 0; j < num_frames; j++) {
+            if (frames[j] == pag) {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            frames[frame_index] = pag;
+            frame_index = (frame_index + 1) % num_frames;
+            falta_de_pag++;
+        }
+    }
+
+    return falta_de_pag;
+}
+
 int main() {
     int reference_string[50];
     gerarStringDeReferencia(reference_string, 50);
